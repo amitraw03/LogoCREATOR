d31fdad056142a881b244aaafa3d2340a13fe78b
amitraw03 <99679149+amitraw03@users.noreply.github.com>
2024-10-27 17:38:56 +0530

    integrate coloricons/imgs availability in the website

--- src/components/IconList.jsx
+++ src/components/IconList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     Dialog,
     DialogContent,
@@ -9,31 +9,50 @@ import {
 } from "@/components/ui/dialog"
 import { icons, Star } from 'lucide-react'
 import { iconList } from '@/constants/icons';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import axios from 'axios';
 
+// API for color icons
+const BASE_URL = 'https://logoexpress.tubeguruji.com'
 
 function IconList({ selectedIcon }) {
     const [openDialog, setOpenDialog] = useState(false);
     const storageValue = JSON.parse(localStorage.getItem('value'));
     const [icon, setIcon] = useState(storageValue ? storageValue?.icon : 'Star');
-    
+    const [pngIconList, setPngIconList] = useState([]);
+
+    useEffect(() => {
+        getPngIcons();
+    }, [])
+
     // setting icon at instance time
-    const Icon =({name,color,size})=>{
+    const Icon = ({ name, color, size }) => {
         const LucidIcon = icons[name];
-        if(!LucidIcon){
+        if (!LucidIcon) {
             return null;
         }
         return <LucidIcon size={size} color={color} />
+    }
+
+    // handling API call
+    const getPngIcons = () => {
+        axios.get(BASE_URL + '/getIcons.php')
+            .then((response) => {
+                const data = response.data;
+                // console.log(data);
+                setPngIconList(data);
+            })
     }
 
     return (
         <div>
             <div>
                 <label>Icon</label>
-                <div onClick= {()=>{setOpenDialog(true)}}
-                className='p-3 cursor-pointer bg-gray-200 hover:bg-gray-300 rounded-md
+                <div onClick={() => { setOpenDialog(true) }}
+                    className='p-3 cursor-pointer bg-gray-200 hover:bg-gray-300 rounded-md
              w-[50px] my-2 flex justify-center items-center'>
-                <Icon name={icon} color='black' size={20} />
-                    
+                { icon?.includes('.png') ? <img src={BASE_URL + '/png/' + icon} alt={icon} />: <Icon name={icon} color='black' size={20} /> }
+
                 </div>
             </div>
             <Dialog open={openDialog} onOpenChange={setOpenDialog}>
@@ -42,15 +61,36 @@ function IconList({ selectedIcon }) {
                     <DialogHeader>
                         <DialogTitle>Select Your favourite Icon</DialogTitle>
                         <DialogDescription>
-                            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 overflow-auto h-[400px] p-6'>
-                                {iconList.map((icon, index) => (
-                                    <div key={index} onClick={() => { setOpenDialog(false); selectedIcon(icon); setIcon(icon) }}
-                                        className='p-3 cursor-pointer bg-gray-200 hover:bg-gray-300 rounded-sm
+                            <Tabs defaultValue="icon" className="w-[400px]">
+                                <TabsList>
+                                    <TabsTrigger value="icon">Icons</TabsTrigger>
+                                    <TabsTrigger value="color-icon">Color-Icons</TabsTrigger>
+                                </TabsList>
+                                <TabsContent value="icon">
+
+                                    <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 overflow-auto h-[400px] p-6'>
+                                        {iconList.map((icon, index) => (
+                                            <div key={index} onClick={() => { setOpenDialog(false); selectedIcon(icon); setIcon(icon) }}
+                                                className='p-3 cursor-pointer bg-gray-200 hover:bg-gray-300 rounded-sm
                                      w-[50px] my-2 flex justify-center items-center'>
-                                        <Icon name={icon} color='black' size={20} />
+                                                <Icon name={icon} color='black' size={20} />
+                                            </div>
+                                        ))}
                                     </div>
-                                ))}
-                            </div>
+                                </TabsContent>
+                                <TabsContent value="color-icon">
+                                    <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 overflow-auto h-[400px] p-6'>
+                                        {pngIconList.map((icon, index) => (
+                                            <div key={index} onClick={() => { setOpenDialog(false); selectedIcon(icon); setIcon(icon) }}
+                                                className='p-3 cursor-pointer bg-gray-200 hover:bg-gray-300 rounded-sm
+                                     w-[50px] my-2 flex justify-center items-center'>
+                                                <img src={BASE_URL + '/png/' + icon} alt={icon} />
+                                            </div>
+                                        ))}
+                                    </div>
+                                </TabsContent>
+                            </Tabs>
+
                         </DialogDescription>
                     </DialogHeader>
                 </DialogContent>
--- src/components/LogoPreview.jsx
+++ src/components/LogoPreview.jsx
@@ -3,7 +3,9 @@ import html2canvas from 'html2canvas';
 import { icons } from 'lucide-react';
 import React, { useContext, useEffect, useState } from 'react'
 
-function LogoPreview({downloadLogo}) { // download logo request from parent component
+
+const BASE_URL = 'https://logoexpress.tubeguruji.com';
+function LogoPreview({ downloadLogo }) { // download logo request from parent component
 
     const [storageValue, setStorageValue] = useState({});
     // use the updated icon/bg controller data from context
@@ -15,81 +17,84 @@ function LogoPreview({downloadLogo}) { // download logo request from parent comp
         console.log(storageData);
         setStorageValue(storageData);
     }, [updateStorage]);
-    
+
     // to let work the download button on gettng download request
     useEffect(() => {
-        if(downloadLogo){
+        if (downloadLogo) {
             downloadLogoHandler();
         }
     }, [downloadLogo]);
 
     //method to download logo
     const downloadLogoHandler = () => {
-         const downloadLink = document.getElementById('downloadLogoDiv');
-         html2canvas(downloadLink,{ backgroundColor: null}).then((canvas) => {
-             const img = canvas.toDataURL('image/png');
-             const link = document.createElement('a');
-             link.download = 'raw_logofy.png';
-             link.href = img;
-             link.click();
-         })
+        const downloadLink = document.getElementById('downloadLogoDiv');
+        html2canvas(downloadLink, { backgroundColor: null }).then((canvas) => {
+            const img = canvas.toDataURL('image/png');
+            const link = document.createElement('a');
+            link.download = 'raw_logofy.png';
+            link.href = img;
+            link.click();
+        })
     };
-    
-    
+
+
     // method for applying background style 
     const getBackgroundStyle = () => {
         const color = storageValue?.bgColor;
-        
+
         if (color?.includes('gradient')) {
-          return {
-            background: color,
-            borderRadius: storageValue?.bgRounded
-          };
+            return {
+                background: color,
+                borderRadius: storageValue?.bgRounded
+            };
         }
-        
+
         return {
-          backgroundColor: color || 'transparent',
-          borderRadius: storageValue?.bgRounded
+            backgroundColor: color || 'transparent',
+            borderRadius: storageValue?.bgRounded
         };
-      };
-       
+    };
 
-     // Fixed Icon rendering component
-     const IconComponent = () => {
-      const IconFromName = icons[storageValue?.icon];
-      
-      if (!IconFromName) {
-          return null;
-      }
 
-      return (
-          <div 
-              className="flex justify-center items-center h-full w-full"
-              style={{
-                  transform: `rotate(${storageValue?.iconRotate || 0}deg)`
-              }}
-          >
-              <IconFromName 
-                  size={storageValue?.iconSize} 
-                  color={storageValue?.iconColor}
-              />
-          </div>
-      );
-  };
+    // Icon/Image rendering component
+    const IconComponent = () => {
+        const IconFromName = icons[storageValue?.icon];
 
-  return (
-      <div className='flex justify-center items-center h-screen'>
-          <div id='downloadLogoDiv'  className=' h-[500px] w-[500px] bg-transparent outline-dotted outline-gray-300'
-           style={{padding: storageValue?.bgPadding}} >
+        if (!IconFromName && !storageValue?.icon?.includes('.png')) {
+            return null;
+        }
 
-              <div className='h-full w-full '
-                  style={getBackgroundStyle()}>
-                  <IconComponent />
-              </div>
-              
-          </div>
-      </div>
-  );
+        return (
+            <div
+                className="flex justify-center items-center h-full w-full"
+                style={{
+                    transform: `rotate(${storageValue?.iconRotate || 0}deg)`
+                }}
+            >
+                {storageValue?.icon?.includes('.png') ?
+                    <img src={BASE_URL + '/png/' + storageValue?.icon} alt="Icon"
+                        style={{ width: storageValue?.iconSize, height: storageValue?.iconSize }} />
+                    : <IconFromName size={storageValue?.iconSize}
+                        color={storageValue?.iconColor} />
+                }
+
+            </div>
+        );
+    };
+
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <div id='downloadLogoDiv' className=' h-[500px] w-[500px] bg-transparent outline-dotted outline-gray-300'
+                style={{ padding: storageValue?.bgPadding }} >
+
+                <div className='h-full w-full '
+                    style={getBackgroundStyle()}>
+                    <IconComponent />
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default LogoPreview;
